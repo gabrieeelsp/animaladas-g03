6a94309a2d01d3c859d877220ad3ad80e648b98b
palmierimayra <palmierimayra@gmail.com>
2024-01-23 21:19:58 -0300

    instalacion de dependencias

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,11 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
 );
+
