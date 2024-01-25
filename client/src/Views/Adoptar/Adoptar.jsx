f932367a70a924db077f9adb0f006670433213fd
Lautaro Rodriguez <taty_0321@hotmail.com>
2024-01-25 19:21:22 -0300

    mejoras de diseño

--- client/src/Views/Adoptar/Adoptar.jsx
+++ client/src/Views/Adoptar/Adoptar.jsx
@@ -1,16 +1,39 @@
 import Cards from "../../Components/Cards/Cards";
-import Navbar from "../../Components/NavBar/NavBar";
+import Footer from "../../Components/Footer/Footer";
+import "./Adoptar.css";
+
 
 const Adoptar = () => {
 
-    return (
-  
-      <div>
-        <Navbar/>
-        <Cards/>
-  
+  return (
+
+    <>
+      <div className="img-container">
+        <img className="banner-img" src="https://img.freepik.com/psd-gratis/retrato-grupo-adorables-cachorros_53876-73962.jpg?w=1380&t=st=1706220732~exp=1706221332~hmac=c50f7966a6d0a2ce902d40c6f7d32e23a16d4877a457de858e07738a4e912c62" alt="dogs-image"/>
+      
+      <div className="button-container-1" >
+        <button type="button" className="btn btn-dark btn-sm" style={{ width: "50px" }}>Reset</button>
       </div>
-    );
-  };
-  
-  export default Adoptar;
+      </div>
+
+      <div className="row" style={{ columnGap: "50px" }}>
+        <div className="col-2"><Cards /></div>
+        <div className="col-2"><Cards /></div>
+        <div className="col-2"><Cards /></div>
+        <div className="col-2"><Cards /></div>
+        <div className="col-2">
+          <div className="button-container-2">
+            <button type="button" className="btn btn-dark btn-sm" style={{ width: "50px" }}>Dark</button>
+            <button type="button" className="btn btn-dark btn-sm" style={{ width: "50px" }}>Dark</button>
+            <button type="button" className="btn btn-dark btn-sm" style={{ width: "50px" }}>Dark</button>
+          </div>
+        </div>
+
+      </div>
+
+        <Footer/>
+    </>
+  );
+};
+
+export default Adoptar;
