cf245cdfc10a578ff2c79ec70ccc3ee9622dd268
palmierimayra <palmierimayra@gmail.com>
2024-01-26 11:45:59 -0300

    login modificado

--- client/src/Views/login/login.jsx
+++ client/src/Views/login/login.jsx
@@ -1,65 +1,21 @@
 import React from "react";
-
-import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom"; // Import Link from react-router-dom
 import logo from "../../img/logoanimaladas.png";
-
 import logo_google from "../../img/logo_google.png";
-import "../../css/login.css";
 
 export default function Login() {
-  const stylecontainer = {
-    width: "25rem",
-  };
-
-  const style_login_icon = {
-    width: "7rem",
-  };
-
-  const style_input = {
-    width: "1rem",
-  };
-
-  const style_remember = {
-    width: "font-size: 0.9rem",
-  };
-
-  const style_text_google = {
-    height: "0.9rem",
-  };
-
-  const google_icon = {
-    height: "1.6rem",
-  };
-
-  const style_background_yellow = {
-    backgroundColor: "#FBD13D",
-  };
-
-  const style_text_yellow = {
-    color: "#FBD13D",
-  };
-
-  const style_background_icon = {
-    color: "white",
-  };
-
   return (
-    <div
-      className="
-     d-flex justify-content-center align-items-center vh-100"
-    >
-      <div className="bg-white p-5 rounded-5  shadow" style={stylecontainer}>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="bg-white p-5 rounded-5 shadow" style={{ width: "25rem" }}>
         <div className="d-flex justify-content-center">
-          <img src={logo} alt="login-icon" style={style_login_icon} />
+          <img src={logo} alt="login-icon" style={{ width: "7rem" }} />
         </div>
         <div>
-          <h1 className="text-center fs-1 fw-bold" style={style_text_yellow}>
-            Login
-          </h1>
+          <h1 className="text-center fs-1 fw-bold text-warning">Login</h1>
         </div>
         <div className="input-group mt-4">
-          <div className="input-group-text  " style={style_background_yellow}>
-            <i class="bi bi-person" style={style_background_icon}></i>
+          <div className="input-group-text bg-warning text-white">
+            <i className="bi bi-person"></i>
           </div>
           <input
             className="form-control bg-light"
@@ -68,8 +24,8 @@ export default function Login() {
           />
         </div>
         <div className="input-group mt-1">
-          <div className="input-group-text " style={style_background_yellow}>
-            <i class="bi bi-lock" style={style_background_icon}></i>
+          <div className="input-group-text bg-warning text-white">
+            <i className="bi bi-lock"></i>
           </div>
           <input
             className="form-control bg-light"
@@ -80,45 +36,45 @@ export default function Login() {
         <div className="d-flex justify-content-around mt-1">
           <div className="d-flex align-items-center gap-1">
             <input className="form-check-input" type="checkbox" />
-            <div className="pt-1" style={style_remember}>
-              Remember me
-            </div>
+            <div className="pt-1">Remember me</div>
           </div>
           <div className="pt-1">
             <a
               href="#"
-              className="text-decoration-none  fw-semibold fst-italic"
-              style={style_text_yellow}
+              className="text-decoration-none fw-semibold fst-italic text-warning"
             >
               Forgot your password?
             </a>
           </div>
         </div>
-        <div
-          className="btn  text-white w-100 mt-4 fw-bold shadow-sm"
-          style={style_background_yellow}
-        >
+        <div className="btn text-white w-100 mt-4 fw-bold shadow-sm bg-warning">
           Login
         </div>
         <div className="d-flex gap-1 justify-content-center mt-1">
           <div>Don't have an account?</div>
           <a
             href="#"
-            className="text-decoration-none  fw-semibold"
-            style={style_text_yellow}
+            className="text-decoration-none fw-semibold text-warning"
           >
             Register
           </a>
         </div>
         <div className="p-3">
-          <div className="border-bottom text-center" style={style_text_google}>
+          <div className="border-bottom text-center text-warning">
             <span className="bg-white px-3">or</span>
           </div>
         </div>
         <div className="btn d-flex gap-2 justify-content-center border mt-3 shadow-sm">
-          <img src={logo_google} alt="google-icon" style={google_icon} />
-          <div className="fw-semibold text-secondary">Continue with Google</div>
+          <img src={logo_google} alt="google-icon" style={{ height: "1.6rem" }} />
+          <div className="fw-semibold text-secondary">
+            Continue with Google
+          </div>
         </div>
+        <NavLink to="/">
+        <div className="btn text-white w-100 mt-4 fw-bold shadow-sm bg-warning">
+        Return to Home
+        </div>
+        </NavLink>
       </div>
     </div>
   );
