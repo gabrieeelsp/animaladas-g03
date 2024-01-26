8b350606bfe3e84adc33e947d2a09fb0466eaf49
palmierimayra <palmierimayra@gmail.com>
2024-01-26 19:06:48 -0300

    cambios en view

--- client/src/Views/Rescatado/Rescatado.jsx
+++ client/src/Views/Rescatado/Rescatado.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import CardR from "../../Components/CardR/CardR";
 
 export default function Rescatado() {
   return (
-<div>
-    
-</div>
-    );
+    <div>
+      <div>
+        <div class="container my-3">
+            <div className="row w-100">
+              <div className="col">
+                <CardR />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 }
