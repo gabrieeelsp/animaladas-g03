3b87c954bf58ff49dd02617adb4df7789c41ef0f
Zackiel1 <alejandrozfm132@gmail.com>
2024-01-25 22:20:47 -0300

    Empece a crear las rutas de user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const userRouter = require('./userRouter');
 
 const router = Router();
 
@@ -7,6 +8,8 @@ const router = Router();
 router.get('/animals', (req, res) => {
     res.send('Lista de animales');
 })
+
+router.use("/user", userRouter);
 
 
 module.exports = router;
