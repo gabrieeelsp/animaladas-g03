14e916c5be0770fd98dd942e7fb44b33a635ec5d
india15 <florenciafarfanbusquets@gmail.com>
2024-01-29 15:42:34 -0300

    Tu mensaje de commit

--- client/src/Views/Donar/donar.jsx
+++ client/src/Views/Donar/donar.jsx
@@ -1,106 +1,102 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Donar.css"; 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+import "./Donar.css";
+
+// Agrega el ícono de la pata a la biblioteca
+library.add(faPaw);
 
 const Donar = () => {
   return (
     <div className="container donar-container">
-    <div className="card-body">
-    <h2 className="text-warning banner-heading">
-    ¿Cuánto quieres aportar?
-</h2>
       <div className="row mt-4">
+        <div className="col-md-6 mb-4 mt-5">
+          <img
+            src="https://img.freepik.com/fotos-premium/perro-gato-sobre-fondo-amarillo_875722-12107.jpg?w=740"
+            className="img-fluid rounded"
+            style={{ border: "2px solid #ffc107" }}
+            alt="Animales"
+          />
+        </div>
 
+        <div className="col-md-6 mb-4 mt-5">
+          <div className="card h-100 donar-custom-card home-card">
+            <div className="card-donar-body d-flex flex-column align-items-center">
+              <h2 className="text-warning banner-heading mt-3 mb-4">
+                ¿Cuánto quieres aportar?
+              </h2>
 
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 donar-custom-card home-card">
-              <div className="card-donar-body d-flex flex-column">
-                <h5 className="card-donar-title">Aporte Pequeño</h5>
-                <p className="card-donar-text">Para ayudar con pequeñas contribuciones.</p>
-                <div className="donar-options mt-auto">
-                  <Link to="/procesar-pago">
-                    <button className="btn btn-warning donar-custom-button home-button">
-                      Aportar $1000
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
-                    <button className="btn btn-warning donar-custom-button-transparent home-button">
-                      Aportar $1500
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
-                  <button className="btn btn-warning donar-custom-button-transparent home-button">
-                      Aportar $2000
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
+              <div className="donar-options d-flex flex-wrap justify-content-center">
+                {/* Aportes Pequeños */}
+                <Link to="/procesar-pago">
                   <button className="btn btn-warning donar-custom-button home-button">
-                      Aportar $2500
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 donar-custom-card home-card">
-              <div className="card-donar-body d-flex flex-column">
-                <h5 className="card-donar-title">Aporte Mediano</h5>
-                <p className="card-donar-text">Realiza una contribución de tamaño intermedio.</p>
-                <div className="donar-options mt-auto">
-                  <Link to="/procesar-pago">
-                  <button className="btn btn-warning donar-custom-button home-button">
-                      Aportar $3000
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
+                    Aportar $1000
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
                   <button className="btn btn-warning donar-custom-button-transparent home-button">
-                      Aportar $3500
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
-                    <button className="btn btn-warning donar-custom-button-transparent home-button">
-                      Aportar $4000
-                    </button>
-                  </Link>
-                  <Link to="/procesar-pago">
-                    <button className="btn btn-warning donar-custom-button home-button">
-                      Aportar $4500
-                    </button>
-                  </Link>
+                    Aportar $1500
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button home-button">
+                    Aportar $2000
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button-transparent home-button">
+                    Aportar $2500
+                  </button>
+                </Link>
+
+                {/* Aportes Medianos */}
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button home-button">
+                    Aportar $3000
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button-transparent home-button">
+                    Aportar $3500
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button home-button">
+                    Aportar $4000
+                  </button>
+                </Link>
+                <Link to="/procesar-pago">
+                  <button className="btn btn-warning donar-custom-button-transparent home-button">
+                    Aportar $5000
+                  </button>
+                </Link>
+
+                {/* Aporte Personalizado */}
+                <a
+                  href="URL_DE_TU_ENLACE_DE_MERCADO_PAGO"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="btn btn-warning donar-custom-button home-button">
+                    Personalizado
+                  </button>
+                </a>
+
+                <div className="gratitude-message mt-4">
+                  <p>
+                    ¡Agradecemos tu generosidad{" "}
+                    <FontAwesomeIcon icon={faPaw} />!
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-
-     
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 donar-custom-card home-card">
-              <div className="card-donar-body d-flex flex-column">
-                <h5 className="card-donar-title">Aporte Personalizado</h5>
-                <p className="card-donar-text">Puedes aportar la cantidad que quieras o puedas.</p>
-                <div className="donar-options mt-auto d-flex justify-content-center"> {/* Agrega las clases d-flex y justify-content-center */}
-                  <a
-                    href="URL_DE_TU_ENLACE_DE_MERCADO_PAGO"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button className="btn btn-warning donar-custom-button home-button">
-                      Aportar
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Donar;
-
-
