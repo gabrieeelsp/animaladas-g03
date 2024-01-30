e96ac53d8e323e251224c6e5733f2623c2518552
fgarces06 <fagarces06@gmail.com>
2024-01-30 18:45:30 -0500

    30/01/2024 antes del main

--- api/src/handlers/userHandlers.js
+++ api/src/handlers/userHandlers.js
@@ -1,5 +1,5 @@
 const createUser = require('../controllers/user/createUser');
-
+const loginUser = require('../controllers/user/loginuser');
 const postUserHandler = async (req, res) => {
     const { name, lastName, email, password, phone, address, imageProfile } =
         req.body;
@@ -21,7 +21,19 @@ const postUserHandler = async (req, res) => {
         res.status(400).json(error.message);
     }
 };
+const loginUserHandler = async (req, res) => {
+    const { email, password } = req.body;
 
+    const emailToLowerCase = email.toLowerCase();
+
+    try {
+        const newUser = await loginUser(email, password);
+        res.status(200).json(newUser);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+};
 module.exports = {
     postUserHandler,
+    loginUserHandler,
 };
