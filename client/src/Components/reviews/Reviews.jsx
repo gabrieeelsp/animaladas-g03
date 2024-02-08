1ba4d810720ff114c2510264d1f449a12e81292e
fgarces06 <fagarces06@gmail.com>
2024-02-08 13:53:46 -0500

    1:53pm 08 feb 2024

---
+++ client/src/Components/reviews/Reviews.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Reviews() {
+  return (
+    <div>
+      <section id="testimonials">
+        <div className="testimonial-heading">
+          <span>Comentarios</span>
+          <h1>Las personas dicen</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
