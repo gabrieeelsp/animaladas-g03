c4639fd799f53f429a6dce22f08c9a147720eb9c
fgarces06 <fagarces06@gmail.com>
2024-02-08 10:19:30 -0500

    mejoramiento de las validaciones y preview userimg

--- client/src/Views/Register/validation_user.js
+++ client/src/Views/Register/validation_user.js
@@ -4,17 +4,20 @@ const Error = {
   email: "",
   phone: "",
   address: "",
+  priority_fields: "",
+  missing_fields: true,
   showerror_name: false,
   showerror_lastName: false,
   showerror_phone: false,
   showerror_address: false,
+  showerror_priority_fields: false,
 };
 
 export default function validateform(data) {
   var regexEmail =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  if (data.name.length > 12) {
+  if (data.name.length > 15) {
     Error.name = "El campo nombre no puede ser mayor a 12 caracteres";
     Error.showerror_name = true;
   } else {
@@ -37,21 +40,44 @@ export default function validateform(data) {
   }
 
   const numericphone = Number(data.phone);
-
+  console.log("valor de numericphone", numericphone);
   if (Number.isNaN(numericphone)) {
+    console.log("ingreso al condicional if");
     Error.phone = "Debe ingresar un número.";
     Error.showerror_phone = true;
+    console.log("valor de errro en el if", Error);
   } else {
+    console.log("ingreso al condicional else");
     Error.phone = "";
     Error.showerror_phone = false;
   }
 
-  if (data.address.length > 12) {
+  if (data.address.length > 20) {
     Error.address = "El campo direccion no puede ser mayor a 12 caracteres";
-    Error.showerror_addresselse = true;
+    Error.showerror_address = true;
   } else {
-    Error.phone = "";
-    Error.showerror_phone = false;
+    Error.address = "";
+    Error.showerror_address = false;
+  }
+  if (
+    (Error.name === "" &&
+      Error.lastName === "" &&
+      Error.email === "" &&
+      Error.phone === "" &&
+      Error.address === "" &&
+      Error.priority_fields === "" &&
+      data.name !== "" &&
+      data.lastName !== "" &&
+      data.email !== "" &&
+      data.phone !== "" &&
+      data.address !== "",
+    data.password !== "")
+  ) {
+    console.log("ingreso a condicional mission fields");
+    Error.missing_fields = false;
+    console.log("valor de error mission fileds", Error);
+  } else {
+    Error.missing_fields = true;
   }
 
   return Error;
