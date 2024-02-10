21b74b963b3c9d28af3ddadcb51533fd91022ae3
india15 <florenciafarfanbusquets@gmail.com>
2024-02-09 21:56:33 -0300

    view pago aprobado

---
+++ client/src/Views/Donar/PagoAprobado.jsx
@@ -0,0 +1,12 @@
+const PagoAprobado = () => {
+    return (
+      <div>
+        <h2>Pago Aprobado</h2>
+        <p>¡Gracias por tu donación! Tu pago se ha procesado correctamente.</p>
+        <p>¡Gracias por tu apoyo!</p>
+      </div>
+    );
+  };
+  
+  export default PagoAprobado;
+  
