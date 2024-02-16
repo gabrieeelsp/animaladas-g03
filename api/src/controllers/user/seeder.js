3f13917fa2a968a558285a66b837ad0d6610203c
NicolasCuello74 <nicocuello7@gmail.com>
2024-02-16 19:16:29 -0300

    se agregaron datos a la tabla de reviews

--- api/src/controllers/user/seeder.js
+++ api/src/controllers/user/seeder.js
@@ -1,4 +1,4 @@
-const { User } = require('../../db');
+const { User, Review } = require('../../db');
 
 const items = [
     {
@@ -48,6 +48,45 @@ const items = [
     },
 ];
 
+const reviews = [
+    {
+        user_name: 'Gabriel',
+        user_lastName: 'Batistuta',
+        user_img:
+            'https://res.cloudinary.com/dwgufqzjd/image/upload/v1708039507/Proyecto_animaladas/default/Foto_Perfil__kaihwx.jpg',
+        score: 4,
+        comment: '¡Excelente producto!',
+        userId: 1,
+    },
+    {
+        user_name: 'Omar',
+        user_lastName: 'Palma',
+        user_img:
+            'https://res.cloudinary.com/dwgufqzjd/image/upload/v1708039507/Proyecto_animaladas/default/foto_perfil_2_cqrzyj.jpg',
+        score: 5,
+        comment: 'Muy buen servicio al cliente.',
+        userId: 2,
+    },
+    {
+        user_name: 'Claudio',
+        user_lastName: 'Caniggia',
+        user_img:
+            'https://res.cloudinary.com/dwgufqzjd/image/upload/v1708039506/Proyecto_animaladas/default/fotoperfil4_ksiccw.jpg',
+        score: 3,
+        comment: 'Podrían mejorar la entrega.',
+        userId: 3,
+    },
+    {
+        user_name: 'Hernan',
+        user_lastName: 'Crespo',
+        user_img:
+            'https://res.cloudinary.com/dwgufqzjd/image/upload/v1708039506/Proyecto_animaladas/default/fotoperfil6_h5vs4g.webp',
+        score: 2,
+        comment: 'No quedé satisfecho con el producto.',
+        userId: 4,
+    },
+];
+
 module.exports = async () => {
     await User.destroy({
         where: {},
@@ -55,4 +94,5 @@ module.exports = async () => {
     });
 
     await User.bulkCreate(items);
+    await Review.bulkCreate(reviews);
 };
