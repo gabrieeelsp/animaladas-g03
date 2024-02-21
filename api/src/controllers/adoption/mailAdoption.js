711bd0e2c22c22aafd36004dd0b18691a00f5658
Zackiel1 <102319575+Zackiel1@users.noreply.github.com>
2024-02-21 10:52:35 -0300

    Update mailAdoption.js
    acomodando un bug

--- api/src/controllers/adoption/mailAdoption.js
+++ api/src/controllers/adoption/mailAdoption.js
@@ -1,18 +1,12 @@
 const { templateSendMail } = require('../../services/nodemailer');
-const findOneById = require('./findOneById');
 
-const mailAdoption = async (id, messageMail) => {
-    const data = await findOneById(id);
-
-    const userEmail = data.user.dataValues.email;
-    const userName = data.user.dataValues.name;
-
+const mailAdoption = async (message, userEmail) => {
     const mail = {
         from: process.env.GOOGLE_EMAIL,
         to: userEmail,
         subject: 'Informe de adopcion',
         title: 'Informe de adopcion',
-        message: `Hola ${userName}, ${messageMail}`,
+        message,
     };
 
     const emailSend = await templateSendMail(mail);
