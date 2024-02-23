51a7d0f9a2611a5b2bcfb54836c9cf91a97f445a
Zackiel1 <102319575+Zackiel1@users.noreply.github.com>
2024-02-23 13:57:41 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
         'Access-Control-Allow-Headers',
